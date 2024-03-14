66229a7a94f58ef445a533797b31cb21e4b88e29
Kafeel Kazeem <kafeelkazeem@gmail.com>
2024-03-14 11:50:25 +0100

    made changes

---
+++ customer_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `handypro`.`signup` (`id` INT(100) NOT NULL AUTO_INCREMENT , `fullName` VARCHAR(100) NOT NULL , `email` VARCHAR(100) NOT NULL , `phoneNumber` VARCHAR(100) NOT NULL , `localArea` VARCHAR(100) NOT NULL , `address` VARCHAR(100) NOT NULL , `password` VARCHAR(100) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
